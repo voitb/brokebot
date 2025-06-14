6b18feef651497b67ed27d0ef184e759482457af
voitb <bartoszwojtczak99@gmail.com>
2025-06-14 18:57:21 +0200

    fix errors

--- src/components/dialogs/settings/components/index.ts
+++ src/components/dialogs/settings/components/index.ts
@@ -1,5 +1,3 @@
 export { GeneralTab } from './GeneralTab';
 export { PrivacyTab } from './PrivacyTab'; 
 export { PricingInteraction } from './PricingInteraction';
-export { LocalUserBillingTab } from './LocalUserBillingTab';
-export { LoggedUserBillingTab } from './LoggedUserBillingTab'; 
